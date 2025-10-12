0a26a8af6f91106c3a0eb660f6bca19591a85b64
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-12 11:44:56 +0900

    add search component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Signup from '../../components/Signup'
 import Login from '../../components/Login'
+import Search from '../../components/Search'
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
 
-      <h2>🙈Search without login🙈</h2>
-
+      <h5>🙈Search without login🙈</h5>
+      <Search/>
       <Signup/>
       <Login/>
 
