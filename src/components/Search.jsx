e865ddc63a3bb6f42cacc36d259720077bb5933e
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-12 17:38:34 +0900

    undo handle search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,44 +3,61 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function Search() {
-  const [word, setWord] = useState("");
-  const [results, setResults] = useState([]);
+  const [typedWord, setTypedWord] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  // const [word, setWord] = useState("");
+  // const [phonetic, setPhonetic] = useState("");
+  // const [partOfSpeech, setPartOfSpeech] = useState("");
+  // const [definition, setDefinition] = useState("");
+  // const [definitions, setDefinitions] = useState({});
+  // const [example, setExample] = useState("");
+  // const [synonyms, setSynonyms] = useState([]);
+  // const [antonyms, setAntonyms] = useState([]);
 
   const handle_search= async() => {
 
-      const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+      const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${typedWord}`);
       console.log(res.data);
-      // console.log(`antonyms: ${res.data[0].antonyms}`);
-      // console.log(`definitions: ${res.data[0].meanings[0].definitions}`);
-      // console.log(`partOfSpeech: ${res.data[0].meanings[0].partOfSpeech}`);
 
-      res.data.map((item)=>{
-        console.log(`word: ${item.word}`);
-        item.meanings.map((meaning,index)=>{
-          console.log(`<${index + 1}>`);
-          console.log(`partOfSpeech: ${meaning.partOfSpeech}`);
-          meaning.definitions.map((def,index)=>{
-            console.log(index + 1);
-            console.log(`definition: ${def.definition}`);
-            console.log(`example: ${def.example}`);
-            console.log(`synonyms: ${def.synonyms}`);
-            console.log(`antonyms: ${def.antonyms}`);
-          })
-        })
-      })
+            if (!res.data || res.data.length === 0) {
+              setSearchResults(["No results found"]);
+              return;
+            }
+            else
+            {
+              setSearchResults(res.data);
+            }
 
-      if (!res.data || res.data.length === 0) {
-        setResults(["No results found"]);
-        return;
-      }
-      else {
-        // Display the first definition of the first meaning
-        setResults(
-          res.data[0].meanings[0].definitions[0].definition
-        );
-      }
+  }
 
-    }
+      // res.data.map((item)=>{
+      //   // Word,Phonetic
+      //   console.log(`word: ${item.word}`);
+      //   console.log(`phonetic: ${item.phonetic}`);
+
+      //   // PartOfSpeech
+      //   item.meanings.map((meaning,index)=>{
+      //     console.log(`<${index + 1}>`);
+      //     console.log(`partOfSpeech: ${meaning.partOfSpeech}`);
+      //     // Definitions
+      //     // meaning.definitions is an array
+      //     // so need to map again
+      //     // to get each definition
+      //     // Each definition has
+      //     // Definitions,Example,Synonyms,Antonyms
+      //     meaning.definitions.map((def,index)=>{
+      //       console.log(index + 1);
+      //       console.log(`definition: ${def.definition}`);
+      //       console.log(`example: ${def.example}`);
+      //       console.log(`synonyms: ${def.synonyms}`);
+      //       console.log(`antonyms: ${def.antonyms}`);
+      //     })
+      //   })
+      // })
+
+      // (prevDefs) => [...prevDefs, def.definition]
+
 
 
   return (
@@ -48,12 +65,12 @@ export default function Search() {
       <h2>Search</h2>
       <input type="text"
         placeholder="Type a word to search"
-        value = {word}
-        onChange={(e)=>setWord(e.target.value)} />
+        value = {typedWord}
+        onChange={(e)=>setTypedWord(e.target.value)} />
       <button onClick={handle_search}>Search</button>
       <div>
         <h3>Search Results:</h3>
-        <p>{results}</p>
+        <p>{}</p>
       </div>
     </>
 
