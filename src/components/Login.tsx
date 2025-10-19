0637c27744fba98f0461bab9b9fd59c526617439
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-10-19 20:37:29 +0900

    form login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,14 +2,41 @@ import { useState, type FormEvent } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 // shadcn-ui
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 import { Button } from "@/components/ui/button"
 
+
+// Zod schema for form validation
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+})
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
+
+  // React Hook Form setup with Zod resolver
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
