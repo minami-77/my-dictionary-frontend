800696f841c996b902de4e07c0a4ba2bfa1d2354
minami takayama <142367451+minami-77@users.noreply.github.com>
2025-11-04 18:37:21 +0900

    can display all info of eeach word

--- src/pages/WordsDetails.tsx
+++ src/pages/WordsDetails.tsx
@@ -1,6 +1,8 @@
+import { Button } from '@/components/ui/button';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router";
+
 
 
 const WordsDetails = () => {
@@ -9,21 +11,19 @@ const WordsDetails = () => {
     note: string
     user: string
     status: number
+    pronunciation:string
     created_at: string
     updated_at: string
-    word:{
-      spelling:string
-      pronunciation:string
-      part_of_speeches:{
-        part_of_speech: string
-        definitions:{
-          definition:string
-          example: string|null
-          synonyms: string|null
-          antonym: string|null
-        }
-      }
-    }
+    spelling:string
+    part_of_speeches:{
+      part_of_speech: string,
+      definitions:{
+        definition:string|null,
+        example: string|null,
+        synonyms: string[]|null,
+        antonyms: string[]|null
+      }[]
+    }[]
   }
 
   const {word}= useParams();
@@ -66,12 +66,39 @@ const WordsDetails = () => {
   },[])
 
   return (
-    <div>
-      <h2>Details of the word</h2>
-      <h2>{word}</h2>
-      <h2>{wordDetails?.created_at}</h2>
-
-    </div>
+    <>
+      <div>
+        <p>Details of the word</p>
+        <h1>{word}</h1>
+        <p>{wordDetails?.pronunciation}</p>
+        <p>{wordDetails?.status}</p>
+        <p>{wordDetails?.note}</p>
+        <p>{wordDetails?.created_at}</p>
+        <p>{wordDetails?.updated_at}</p>
+        {wordDetails?.part_of_speeches &&
+              wordDetails.part_of_speeches.map((pos,i)=>(
+                // wrapper needed (Each .map() returns a single parent element.)
+                <div key={i}>
+                  <h1>{pos.part_of_speech}</h1>
+                  <ol>
+                    {pos.definitions.map((defn,j)=>(
+                      // wrapper needed (Each .map() returns a single parent element.)
+                      <li key={j}>
+                        <div>{defn.definition}</div>
+                        <div>{defn.example&& defn.example}</div>
+                        <div>{defn.synonyms&& defn.synonyms.join(", ")}</div>
+                        <div>{defn.antonyms&& defn.antonyms.join(", ")}</div>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              ))
+        }
+      </div>
+      <div>
+        <Button>Back</Button>
+      </div>
+    </>
   );
 };
 
